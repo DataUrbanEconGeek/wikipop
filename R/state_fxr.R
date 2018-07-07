5651e52669190e26e8bd9d500b3a599337bb9426
PunkFood-Disme <andrew.a.rosa@gmail.com>
2018-07-06 23:29:21 -0400

    updating state_fxr description

--- R/state_fxr.R
+++ R/state_fxr.R
@@ -1,5 +1,9 @@
-#' A function to fix the state's name for wikipedia link
+#' Prepare state's name for wikipedia link
+#'
+#' @param x A character string of a state's name
 #' @export state_fxr
+#' @examples
+#' state_fxr("New York")
 
 state_fxr <- function(x){
         paste0(",_", gsub(" ", "_", x))
